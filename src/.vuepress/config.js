68d884365a143bad3c61fc3120e24002c9fb81ae
ruki <waruqi@gmail.com>
2018-11-07 23:36:59 +0800

    add gitalk

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   dest: 'docs',
   head: [
-    ['link', { rel: 'shortcut icon', type: "image/x-icon", href: `/favicon.ico` }]
+    ['link', { rel: 'shortcut icon', type: "image/x-icon", href: `/favicon.ico` }],
   ],
   locales: {
     '/': {
